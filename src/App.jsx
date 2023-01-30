d03ed69a1a3b718b5daed4dc85670d50a79487bb
Yasmin Ramadini <editfotobgr@gmail.com>
2023-01-30 09:46:33 +0700

    menambahkan deskripsi

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <main>
+    <div>
       <h1>App React + GitHub</h1>
       <p>Tutorial React + Github agar kolaborasi lebih baik</p>
     </div>
-  )
+  );
 }
 
 export default App;
